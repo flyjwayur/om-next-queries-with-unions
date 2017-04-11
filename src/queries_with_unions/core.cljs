6d256c46a05d84d642688d8a275b1f51fdb52070
HyeSoo <flyjwayur@gmail.com>
2017-04-11 17:03:43 +0300

    Add render implementations and parser

--- src/queries_with_unions/core.cljs
+++ src/queries_with_unions/core.cljs
@@ -6,7 +6,6 @@
 
 (enable-console-print!)
 
-
 (def init-data
   {:dashboard/items
    [{:id 0 :type :dashboard/post
@@ -37,17 +36,43 @@
 (defui Post
   static om/IQuery
   (query [this]
-    [:id :type :title :author :content]))
+    [:id :type :title :author :content])
+  Object
+  (render [this]
+    (let [{:keys [title author content] :as props} (om/props this)]
+      (dom/div nil
+               (dom/h3 nil title)
+               (dom/h4 nil author)
+               (dom/p nil content)))))
+
+(def post (om/factory Post))
 
 (defui Photo
   static om/IQuery
   (query [this]
-    [:id :type :title :image :caption]))
+    [:id :type :title :image :caption])
+  Object
+  (render [this]
+    (let [{:keys [title image caption]} (om/props this)]
+      (dom/div nil
+               (dom/h3 nil (str "Photo: " title))
+               (dom/div nil image)
+               (dom/p nil (str "Caption: " caption))))))
+
+(def photo (om/factory Photo))
 
 (defui Graphic
   static om/IQuery
   (query [this]
-    [:id :type :title :image]))
+    [:id :type :title :image])
+  Object
+  (render [this]
+    (let [{:keys [title image]} (om/props this)]
+      (dom/div nil
+               (dom/h3 nil (str "Graphic: " title))
+               (dom/div nil image)))))
+
+(def graphic (om/factory Graphic))
 
 (defui DashboardItem
   static om/Ident
@@ -60,9 +85,57 @@
       (map #(conj % :favorites)
            [(om/get-query Post)
             (om/get-query Photo)
-            (om/get-query Graphic)]))))
+            (om/get-query Graphic)])))
+  Object
+  (render [this]
+    (let [{:keys [id type favorites] :as props} (om/props this)]
+      (dom/li
+        #js {:style #js {:padding 10 :borderBottom "1px solid black"}}
+        (dom/div nil
+                 (({:dashboard/post    post
+                    :dashboard/photo   photo
+                    :dashboard/graphic graphic} type)
+                   (om/props this)))
+        (dom/div nil
+                 (dom/p nil (str "Favorites: " favorites))
+                 (dom/button
+                   #js {:onClick
+                        (fn [e]
+                          (om/transact! this
+                                        `[(dashboard/favorite {:ref [~type ~id]})]))}
+                   "Favorite!"))))))
+
+(def dashboard-item (om/factory DashboardItem))
 
 (defui Dashboard
   static om/IQuery
   (query [this]
-    [{:dashboard/items (om/get-query DashboardItem)}]))
+    [{:dashboard/items (om/get-query DashboardItem)}])
+  Object
+  (render [this]
+    (let [{:keys [dashboard/items]} (om/props this)]
+      (apply dom/ul
+             #js {:style #js {:padding 0}}
+             (map dashboard-item items)))))
+
+(defmulti read om/dispatch)
+
+(defmethod read :dashboard/items
+  [{:keys [state]} k _]
+  (let [st @state]
+    {:value (into [] (map #(get-in st %)) (get st k))}))
+
+(defmulti mutate om/dispatch)
+
+(defmethod mutate 'dashboard/favorite
+  [{:keys [state]} k {:keys [ref]}]
+  {:action
+   (fn []
+     (swap! state update-in (conj ref :favorites) inc))})
+
+(def reconciler
+  (om/reconciler
+    {:state  init-data
+     :parser (om/parser {:read read :mutate mutate})}))
+
+(om/add-root! reconciler Dashboard (gdom/getElement "app"))
